f41216ed952fc717408fac7dccf8feb5918df7b7
Abhishek Suman <abhisheksuman000@gmail.com>
2017-12-03 06:27:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -307,6 +307,7 @@ switch (ENVIRONMENT)
 
 	define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
 
+
 /*
  * --------------------------------------------------------------------
  * LOAD THE BOOTSTRAP FILE
